6c0dc2d4c93caecc6dc9e7e76afe71c385a06b6b
Enigma <74756147+novb1492@users.noreply.github.com>
2022-11-04 12:21:41 +0900

    first

---
+++ components/Tutorial.vue
@@ -0,0 +1,20 @@
+<!-- Please remove this file from your project -->
+<template>
+   <nuxt-link to="/login">로그인</nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+  head: () => ({
+    title: 'users',
+    meta: [
+      {
+        hid: 'description',
+        name: 'og:description',
+        content: 'Users Pages',
+      },
+    ],
+  }),
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,44 @@
+<template lang="">
+    <div>
+        login
+        <div id="test"></div>
+        <input type="email" id="email">
+    </div>
+</template>
+<script>
+import { markRaw } from 'vue';
+import {test} from "../middleware/authenticated.js";
+export default {
+    middleware: test,
+    head: () => ({
+    title: 'login',
+    meta: [
+      {
+        hid: 'description',
+        name: 'og:description',
+        content: 'Users Pages',
+      },
+    ],
+  }),
+  mounted(){
+    window.ClassicEditor
+          .create(document.querySelector('#test'), {
+            extraPlugins: [],
+            toolbar: [],
+          })
+          .then(newEditor => {
+            this.editor = markRaw(newEditor);
+            // this.editor.setData('test');
+            this.editor.enableReadOnlyMode( 'test2');
+    
+          })
+          .catch(error => {
+            console.log(error);
+            alert('에디터생성에 실패했습니다');
+          })
+  }
+}
+</script>
+<style lang="">
+    
+</style>
