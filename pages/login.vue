83dc6f0c01057193362279da537e9720a6b311df
Enigma <74756147+novb1492@users.noreply.github.com>
2022-11-04 12:40:54 +0900

    그밖의 요소들도 가능

--- pages/login.vue
+++ pages/login.vue
@@ -9,18 +9,30 @@
 import { markRaw } from 'vue';
 import {test} from "../middleware/authenticated.js";
 export default {
+    async asyncData(context) {
+      // const data = await context.$axios.$get(...)
+      // `todos` 는 data() 안에 선언되어 있지 않아도 됩니다.
+      return { todos: {title:'login'} };
+      // `todos` 는 local data에 병합(merge)됩니다.
+    },
     middleware: test,
-    head: () => ({
-    title: 'login',
-    meta: [
-      {
-        hid: 'description',
-        name: 'og:description',
-        content: 'Users Pages',
-      },
-    ],
-  }),
+    /*
+      받은 정보 동적으로 title변경
+    */ 
+    head () {
+      return {
+          title: this.todos.title,
+          meta: [
+            {
+              hid: 'description',
+              name: 'og:description',
+              content: 'Users Pages1234',
+            },
+          ],
+      }
+    },
   mounted(){
+    console.log(this.todos);
     window.ClassicEditor
           .create(document.querySelector('#test'), {
             extraPlugins: [],
